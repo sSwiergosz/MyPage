f2e538813d6f5e031acc48162d1e4e12ebfb0ded
sSwiergosz <szymon.swiergosz@gmail.com>
2016-12-10 02:19:39 +0100

    Added downloading news from PubMed and fixed bugs

--- personalApp/views.py
+++ personalApp/views.py
@@ -10,16 +10,29 @@ def contact(request):
 
 def news(request):
 	d = {}
+	term = 'bioinformatics'
+
+	if 'term' in request.GET:
+		term = request.GET['term']
+
+	if term == '':
+		term = 'bioinformatics'
 	
-	Entrez.email='szymon.swiergosz@gmail.com' #always tell who you are!
-	handle = Entrez.esearch(db='pubmed', term='bioinformatics', report='medline', format='text', retmax=5)
+	Entrez.email='guest@email.com' #always tell who you are!
+	handle = Entrez.esearch(db='pubmed', term=term, report='medline', format='text', retmax=5)
 	results = Entrez.read(handle)
-	idlist = results['IdList']
-	handle = Entrez.efetch(db='pubmed', id=idlist, rettype='medline', retmode='text')
-	
-	records = list(Medline.parse(handle))
 
-	for record in records:
-		d[record.get("PMID")] = [record.get("TI"), ', '.join(record.get("AU"))]
+	if results['RetMax'] != '0': #if articles exist, then RetMax has some values
+		idlist = results['IdList']
+		handle = Entrez.efetch(db='pubmed', id=idlist, rettype='medline', retmode='text')
+		records = list(Medline.parse(handle))
+		for record in records:
+			if record.get("AU") in records:
+				d[record.get("TI")] = [', '.join(record.get("AU")), record.get("JT"), record.get("PMID"),]
+			else: #without authors and journals
+				if record.get("TI") is not None: #sometimes even title is missing...
+					d[record.get("TI")] = record.get("PMID")
+	else: #if articles don't exist, then we send empty dictionary
+		d = {}
 
-	return render(request, 'personalApp/news.html', {'articles': d})
+	return render(request, 'personalApp/news.html', {'articles': d, 'search_value': term,})
